2dbfbcbbe63dc33b8acba8b19b157798f0ec9b07
孙达明 <83948628+tyza66@users.noreply.github.com>
2026-06-05 08:55:18 +0800

    Deploy: copy docs/* to root

---
+++ precache-manifest.3e70c39bbdbd8ba77c3ed1b4f591d8ee.js
@@ -0,0 +1,80 @@
+self.__precacheManifest = [
+  {
+    "revision": "b66cbb4e6f8f98eeb093",
+    "url": "/css/Board.944e3187.css"
+  },
+  {
+    "url": "/Board.b66cbb4e6f8f98eeb093.js"
+  },
+  {
+    "revision": "a16d460e63f70ca2b5ee",
+    "url": "/css/app.6215bafa.css"
+  },
+  {
+    "revision": "a16d460e63f70ca2b5ee",
+    "url": "/js/app.77f1ac78.js"
+  },
+  {
+    "revision": "6c721c72442b9562be34",
+    "url": "/css/chunk-vendors.1f0a25b2.css"
+  },
+  {
+    "url": "/chunk-vendors.6c721c72442b9562be34.js"
+  },
+  {
+    "revision": "fa800ab2ad9ce0ababeabe16f84e1655",
+    "url": "/img/mutantcat.fa800ab2.png"
+  },
+  {
+    "revision": "d7cbafd22166ce119605e2b8d8ba2149",
+    "url": "/img/logo.d7cbafd2.png"
+  },
+  {
+    "revision": "143146fa24554ae2c5ac0a3982abb952",
+    "url": "/fonts/ionicons.143146fa.woff2"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "/fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "/fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "/img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "4b730502cdd5e94baeecb1463e13eb2d",
+    "url": "/fonts/iconfont.4b730502.ttf"
+  },
+  {
+    "revision": "e5c9b2aa9ebb118b934ddd5db1875dcb",
+    "url": "/img/iconfont.e5c9b2aa.svg"
+  },
+  {
+    "revision": "ddcacb8b3588771a854d5386bd8ac05d",
+    "url": "/fonts/iconfont.ddcacb8b.eot"
+  },
+  {
+    "revision": "735fb3bc72cb44046632154ffb01c59f",
+    "url": "/fonts/iconfont.735fb3bc.woff"
+  },
+  {
+    "revision": "377373e729212fd45fd0d53f42275cf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "474397281c3cc3cc8f66b56ab00775e2",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "ace3a439b4d36c8c6d2c8f59e6c94aef",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ba2b8887b0120234271efc0723ecad0a",
+    "url": "/sitemap.xml"
+  }
+];
